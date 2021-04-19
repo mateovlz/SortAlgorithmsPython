a878190b79be9bb1c2d7ec547759c787134e1ddf
Mateo Velez <mateo.velez@olsoftware.com>
2021-04-18 22:08:42 -0500

    add: adding comments and new creation of the list to order using random numbers.

--- Main.py
+++ Main.py
@@ -1,36 +1,52 @@
-from Methods import BubbleSort as bubble, QuickSort as quick, MergeSort as merge, SelectionSort as selection, HeapSort as heap
+from Methods import BubbleSort as bubble, QuickSort as quick, MergeSort as merge, SelectionSort as selection, \
+    HeapSort as heap
 import cProfile as profile
+import random
 import sys
 
-maximum = 2000
-array = list(range(0,maximum))
+# It create a list within 3000 registers randomly,
+# with a range from 0 to 3000
+maximum = 3000
+randomList = []
+for i in range(0, maximum):
+    randomList.append(random.randint(0, maximum))
+
+print("List to Order: {}".format(randomList))
+
 
 def quickort():
-    quick.quick_sort(array, 0, len(array) - 1)
-    #print(array)
+    quick.quick_sort(randomList, 0, len(randomList) - 1)
+    # print(randomList)
+
 
 def bubblesort():
-    bubble.bubble_sort(array)
-    #print(array)
+    bubble.bubble_sort(randomList)
+    # print(randomList)
+
 
 def mergesort():
-    merge.merge_sort(array)
-    #print(array)
+    merge.merge_sort(randomList)
+    # print(randomList)
+
 
 def selectionsort():
-    selection.selection_sort(array)
-    #print(array)
+    selection.selection_sort(randomList)
+    # print(randomList)
+
 
 def heapsort():
-    heap.heap_sort(array)
-    #print(array)
+    heap.heap_sort(randomList)
+    # print(randomList)
 
+
+# The main method of the script
 if __name__ == '__main__':
-    sys.setrecursionlimit(maximum +1000)
-    #profile.run('start()')
+    # Setting the limit of recursion that python interpreter could do.
+    sys.setrecursionlimit(maximum + maximum)
+    # profile.run('start()')
     print("--------------------------------------- BubbleSort -------------------------------")
     profile.run('bubblesort()')
-    print("--------------------------------------- SortAlgorithms -------------------------------")
+    print("--------------------------------------- QuickSort -------------------------------")
     profile.run('quickort()')
     print("--------------------------------------- MergeSort -------------------------------")
     profile.run('mergesort()')
